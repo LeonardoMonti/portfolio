2999ee130cb6627ade9ac63a8fc144b116d6c501
Leonardo Monti <contatoleomonti@gmail.com>
2022-01-11 11:18:25 -0300

    adiciona basename

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import NavMenu from './components/NavMenu';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <NavMenu />
       <Switch>
         <Route path="/about">
