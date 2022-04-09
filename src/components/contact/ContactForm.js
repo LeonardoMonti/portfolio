d5e61f411c4fb31e5f9d852509ab6cccfa897c3f
LeonardoMonti <contatoleomonti@gmail.com>
2022-04-09 01:53:47 -0300

    .

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useFormik } from 'formik';
 import styled from 'styled-components';
 import { FaAngleDoubleRight } from 'react-icons/fa';
